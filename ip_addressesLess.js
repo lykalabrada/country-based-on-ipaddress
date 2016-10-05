fbe7b8ea64238e5844976a18ffad312defddaa8c
Lyka Labrada <lykalabrada@gmail.com>
2016-10-05 15:08:10 +0800

    Less IP Address List

---
+++ ip_addressesLess.js
@@ -0,0 +1,18 @@
+var ip_addresses = [{
+"ID":72,
+"BeginningIP":19202048,
+"EndingIP":19267583,
+"CountryName":"Philippines"
+},
+{
+"ID":73,
+"BeginningIP":19267584,
+"EndingIP":19398655,
+"CountryName":"India"
+},
+{
+"ID":74,
+"BeginningIP":19398656,
+"EndingIP":19660799,
+"CountryName":"Australia"
+}];
